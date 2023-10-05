ade22027f1a58b58ab9dd744ab3454ee6318fb79
이찬혁 <109561938+dogbokchif@users.noreply.github.com>
2023-10-05 21:40:28 +0900

    interface 제거

--- src/main/java/pcot/psd/PSDPcot.java
+++ src/main/java/pcot/psd/PSDPcot.java
@@ -1,9 +1,21 @@
 package pcot.psd;
 
 import pcot.psd.entity.Psd;
+import pcot.psd.entity.PsdReader;
+import java.io.*;
 
-import java.io.IOException;
+public class PSDPcot {
+    private final PsdReader psdManipulator;
+    public PSDPcot() {
+        psdManipulator = new PsdReader();
+    }
 
-public interface PSDPcot {
-    public Psd parsingPSD(String path) throws IOException;
+    public Psd parsingPSD(String path) throws IOException {
+        FileInputStream fis = new FileInputStream(path);
+        BufferedInputStream bis = new BufferedInputStream(fis);
+
+        psdManipulator.open(bis);
+        psdManipulator.run();
+        return psdManipulator.getPsd();
+    }
 }
